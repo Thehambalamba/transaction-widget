ecdf0da1e05d6372ff035a97e16da220baf09290
the_prv <pervicnikola@gmail.com>
2025-01-07 11:51:42 +0100

    Create <TransactionLIst />

---
+++ src/components/transaction-list.tsx
@@ -0,0 +1,40 @@
+import type { Transaction } from "../types/transaction";
+import TransactionDetails from "./transaction-details";
+
+type Props = {
+  transactions: Transaction[];
+  isPending: boolean;
+};
+
+function TransactionList({ transactions, isPending }: Props): JSX.Element {
+  return (
+    <div className="flex flex-col gap-2">
+      {transactions.map(
+        ({
+          id,
+          status,
+          value,
+          unit,
+          receiver,
+          sender,
+          approvalProgress,
+          timestamp,
+        }) => (
+          <TransactionDetails
+            key={id}
+            status={status}
+            value={value}
+            unit={unit}
+            receiver={receiver}
+            sender={sender}
+            approvalProgress={approvalProgress}
+            isPending={isPending}
+            timestamp={timestamp}
+          />
+        )
+      )}
+    </div>
+  );
+}
+
+export default TransactionList;
