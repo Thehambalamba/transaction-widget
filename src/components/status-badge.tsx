7dbb28272c8be9794a83509e10159446763192a1
the_prv <pervicnikola@gmail.com>
2025-01-07 11:51:17 +0100

    Create <StatusBadge />

---
+++ src/components/status-badge.tsx
@@ -0,0 +1,24 @@
+import { TransactionStatus } from "../types/enums";
+import type { Transaction } from "../types/transaction";
+
+type Props = Pick<Transaction, "status">;
+
+function StatusBadge({ status }: Props) {
+  const statusStyles = {
+    [TransactionStatus.Approved]: "bg-primaryBlueSky50 text-primaryBlueSky900",
+    [TransactionStatus.Submitted]: "bg-secondarySand200 text-secondarySand950",
+    [TransactionStatus.Rejected]: "bg-primaryOrange50 text-primaryOrange500",
+  };
+
+  return (
+    <div
+      className={`px-3 py-1  rounded-2xl justify-start items-center gap-1 inline-flex ${statusStyles[status]}`}
+    >
+      <p className="text-inherit text-xs font-normal font-['Basis Grotesque Pro'] leading-[14px]">
+        {status}
+      </p>
+    </div>
+  );
+}
+
+export default StatusBadge;
